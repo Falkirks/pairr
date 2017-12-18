38aa17794b112473a99783d33c758bbf64680b42
Falkirks <me@falkirks.com>
2017-12-18 09:04:39 -0800

    Increased file size limit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let sharedState = {
 
 server.listen(process.env.PORT || 3000);
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use( bodyParser.json({limit: '2mb'}) );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
