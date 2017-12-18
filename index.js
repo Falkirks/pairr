8b2719b6e158ad66fd29faa4af7b27514675dd75
Falkirks <me@falkirks.com>
2017-12-18 10:19:16 -0800

    More things!!!

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 let app = require('express')();
-let server = require('http').Server(app);
+let http = require('http');
+let server = http.Server(app);
 let io = require('socket.io')(server);
 let randomstring = require("randomstring");
 let bodyParser = require('body-parser');
+let request = require('request');
 
 let sharedState = {
   serviceOnline: true
+};
+let emailToSock = {
+
 };
 
 server.listen(process.env.PORT || 3000);
@@ -22,8 +27,23 @@ app.get('/', function (req, res) {
 
 app.post('/mailgun', function (req, res) {
   console.dir(req.body);
-  if(req.body.sender && req.body.sender === 'do_not_reply@oldadm.ubc.ca'){
-    console.log(req.body);
+  const socket = emailToSock[req.body.To];
+  if(socket !== undefined){
+    if(req.body.attachments !== undefined){
+      const attachment = JSON.parse(str);
+      console.log("fetching from " + attachment[0].url);
+
+      request({url: 'https://api:' + process.env.MAILGUN_API_KEY + '@' + attachment[0].url}, function (error, response, body) {
+        console.log(body);
+        socket.emit('server-pair-data', {
+          csv: body
+        });
+      });
+
+    }
+    else{
+      socket.emit('server-use-csv', {});
+    }
   }
   res.end('OK');
 });
@@ -34,6 +54,11 @@ io.on('connection', function (socket) {
     console.log("client connected");
   });
   socket.on('client-req-email', function (data) {
-    socket.emit('server-gen-email', {email: randomstring.generate() + "@pairr.falkirks.com"});
-  })
+    // Just ignore collisions ;)
+    const email =  randomstring.generate() + "@pairr.falkirks.com";
+    emailToSock[email] = socket;
+    socket.emit('server-gen-email', {email: email});
+  });
 });
+
+console.log("server listening?...");
